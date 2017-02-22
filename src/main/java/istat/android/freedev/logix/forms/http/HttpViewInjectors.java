a1949de75fd4e2a91c1c86076be90be5846bb4bb
Toukea Tatsi J (Itat) <ttjsoftz@gmail.com>
2017-02-22 23:56:00 +0000

    getting start

---
+++ src/main/java/istat/android/freedev/logix/forms/http/HttpViewInjectors.java
@@ -0,0 +1,8 @@
+package istat.android.freedev.logix.forms.http;
+
+/**
+ * Created by istat on 10/01/17.
+ */
+
+public class HttpViewInjectors {
+}
---
+++ src/main/java/istat/android/freedev/logix/forms/sqlite/SQLiteViewInjectors.java
@@ -0,0 +1,8 @@
+package istat.android.freedev.logix.forms.sqlite;
+
+/**
+ * Created by istat on 10/01/17.
+ */
+
+public class SQLiteViewInjectors {
+}
